ee8e1f2aec94261100d643ada4bec12f5ee7eafb
Shreya <a.shreya0806@gmail.com>
2025-04-30 18:02:30 +0530

    Pushing before fixing conda

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 ## Setup.py is used to install the package
 from setuptools import setup, find_packages
+from typing import List
+
+Hyphen_e_dot = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    # this function will return the list of requirements
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        
+        if Hyphen_e_dot in requirements:
+            requirements.remove(Hyphen_e_dot)
+    return requirements
+        
 
 setup(
     name= 'MLPro',
     version= '0.0.1',
     author= 'Shreya',
     packages=find_packages(),
-    install_requires=['numpy', 'seaborn', 'pandas', 'matplotlib', 'scikit-learn'],
+    install_requires=get_requirements('requirements.txt'),
     
 )
